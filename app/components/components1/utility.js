9f3828e5f39453526f55bbb18b9f820bcbe53524
anders <36933094+tabcat@users.noreply.github.com>
2018-11-10 15:23:08 -0600

    redux

--- app/components/components1/utility.js
+++ app/components/components1/utility.js
@@ -8,6 +8,9 @@ import Button from "@material-ui/core/Button";
 import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 
+import { connect } from 'react-redux';
+import store from '../../store';
+
 var namehash = require('eth-ens-namehash');
 var sha3 = require('js-sha3').keccak_256;
 
@@ -61,93 +64,49 @@ const styles = theme => ({
   }
 });
 
+function hash(string) {
+  return namehash.hash(string);
+}
+
+function labelhash(string) {
+  if (string !== undefined) {
+    return '0x' + sha3(string.split('.')[0]);
+  }
+}
+
+function keccak256(string) {
+  if (string !== undefined) {
+    return '0x' + sha3(string);
+  }
+}
+
 class Utility extends React.Component {
-
-      state = {
-        currentAcc: '',
-        str: '',
-        setStr: (event) => {
-          this.setState({ str: event.target.value})
-        },
-        labelhash: () => {
-          return '0x' + sha3(this.state.str.split('.')[0])
-        },
-        namehash: () =>  {
-          return namehash.hash(this.state.str)
-        },
-        sha3: () => {
-          return '0x' + sha3(this.state.str)
-        }
-
-      }
 
       handleChange(e) {
         let string = namehash.normalize(e.target.value);
-        this.setState({
-          valueSet: string,
-          labelhashValue: '0x' + sha3(string.split('.')[0]),
-          namehashValue: namehash.hash(string)
-        });
+        store.dispatch({
+          type: 'SET_HASHER_INPUT',
+          hasherInput: string
+        })
       }
 
-      checkAcc(obj) {
-        if(!(this.state.currentAcc === obj.selectedAddress)) {
-          this.setState({currentAcc: obj.selectedAddress});
-        }
-      }
-
-      componentDidMount() {
-        try {
-          if(web3.currentProvider !== null) {
-            EmbarkJS.onReady(() => {
-              if (EmbarkJS.isNewWeb3()) {
-
-                this.setState({currentAcc: web3.eth.defaultAccount});
-                web3.currentProvider.publicConfigStore.on('update', obj => this.checkAcc(obj));
-
-              } else {
-                if (EmbarkJS.Messages.providerName === 'whisper') {
-                  console.log(Error(`current web3 api not supported`))
-                } else {
-                  console.log(Error(`web3 provider not detected/mounted`))
-                }
-              }
-            });
-          } else {
-            console.log(Error(`web3 provider not detected/mounted`))
-          }
-        }
-        catch(err) {
-          console.log(err);
-        }
-      }
-
-
-      componentWillUnmount() {
-        try {
-          if (web3.currentProvider !== null){
-
-            web3.currentProvider.publicConfigStore.removeAllListeners();
-
-          }
-        }
-        catch(err) {
-          console.log(err);
-        }
-      }
 
   render() {
 
-    const { classes, theme } = this.props;
+    const { classes, theme, str, currentAcc, networkType } = this.props;
 
     return (
       <div className={classes.root}>
 
         <div className={classes.indent}>
           <div className={classes.field}>
-          <Typography variant="subheading" children="Current Account: " />
-          <Typography variant="subheading" children={this.state.currentAcc} />
-        </div>
+          <Typography variant="subheading" children="Current Account:" />
+          <Typography variant="subheading" children={currentAcc} />
+          </div>
+          <div className={classes.field}>
+          <Typography variant="subheading" children="Network Type:" />
+          <Typography variant="subheading" children={networkType} />
+          </div>
         </div>
 
         <Divider className={classes.space} />
@@ -160,8 +119,8 @@ class Utility extends React.Component {
               <TextField
                 className={classes.textField}
                 placeholder="string to hash"
-                onChange={event => this.state.setStr(event)}
-                value={this.state.str}
+                onChange={event => this.handleChange(event)}
+                value={str}
               />
             </div>
             <div className={classes.spacer} />
@@ -169,19 +128,19 @@ class Utility extends React.Component {
             <div className={classes.output}>
               <div>
                 <Typography variant="caption" children="str" />
-              <Typography className={classes.typo} variant="body1" children={this.state.str} />
+              <Typography className={classes.typo} variant="body1" children={str} />
               </div>
               <div>
                 <Typography variant="caption" children="labelhash" />
-              <Typography className={classes.typo} variant="body1" children={this.state.labelhash()} />
+              <Typography className={classes.typo} variant="body1" children={labelhash(str)} />
               </div>
               <div>
                 <Typography variant="caption" children="namehash" />
-              <Typography className={classes.typo} variant="body1" children={this.state.namehash()} />
+              <Typography className={classes.typo} variant="body1" children={hash(str)} />
               </div>
               <div>
                 <Typography variant="caption" children="keccak256" />
-              <Typography className={classes.typo} variant="body1" children={this.state.sha3()} />
+              <Typography className={classes.typo} variant="body1" children={keccak256(str)} />
               </div>
             </div>
           </div>
@@ -193,9 +152,17 @@ class Utility extends React.Component {
   }
 }
 
+const mapStateToProps = function(store) {
+  return {
+    str: store.toolboxState.hasherInput,
+    currentAcc: store.web3State.currentAcc,
+    networkType: store.web3State.networkType
+  };
+}
+
 Utility.propTypes = {
   classes: PropTypes.object.isRequired,
   theme: PropTypes.object.isRequired
 };
 
-export default withStyles(styles, { withTheme: true })(Utility);
+export default connect(mapStateToProps)(withStyles(styles, { withTheme: true })(Utility));
