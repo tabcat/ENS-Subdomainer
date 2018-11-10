ee974e1428d2816efdca11c6a98b89049d3efa2a
anders <36933094+tabcat@users.noreply.github.com>
2018-11-10 15:22:50 -0600

    redux

--- app/components/components1/toolbox.js
+++ app/components/components1/toolbox.js
@@ -7,6 +7,9 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
+import { connect } from 'react-redux';
+import store from '../../store';
+
 import Utility from './utility'
 
 const styles = theme => ({
@@ -30,24 +33,27 @@ class Toolbox extends React.Component {
     expanded: null,
   };
 
-  handleChange = panel => (event, expanded) => {
-    this.setState({
-      expanded: expanded ? panel : false,
-    });
-  };
+  // handleChange = panel => (event, expanded) => {
+  //   this.setState({
+  //     expanded: expanded ? panel : false,
+  //   });
+  // };
 
-  handleChange = panel => (event, expanded) => {
-   this.setState({
-     expanded: expanded ? panel : false,
-   });
+  expandToggle = panel => (event, expanded) => {
+   // this.setState({
+   //   expanded: expanded ? panel : false,
+   // });
+   store.dispatch({
+     type: 'TOGGLE_EXPAND',
+     expanded: expanded ? panel : false
+   })
  };
 
   render(){
-    const { classes } = this.props;
-    const { expanded } = this.state;
+    const { classes, expanded } = this.props;
 
     return (
-      <ExpansionPanel expanded={expanded === 'panel1'} onChange={this.handleChange('panel1')}>
+      <ExpansionPanel expanded={expanded === 'panel1'} onChange={this.expandToggle('panel1')}>
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
             <Typography className={classes.heading}>ToolBox</Typography>
             <Typography className={classes.secondaryHeading}>Hashing utilities and more! (mostly just hashing utilities)</Typography>
@@ -60,8 +66,14 @@ class Toolbox extends React.Component {
   }
 }
 
+const mapStateToProps = function(store) {
+  return {
+    expanded: store.toolboxState.expanded
+  };
+}
+
 Toolbox.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(Toolbox);
+export default connect(mapStateToProps)(withStyles(styles)(Toolbox));
