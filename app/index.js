d829eceb4ca71f0da4518da5b0874ffc6a8dcc31
anders <36933094+tabcat@users.noreply.github.com>
2018-11-10 15:16:01 -0600

    redux

--- app/index.js
+++ app/index.js
@@ -1,8 +1,17 @@
 import React from "react";
-import {render} from "react-dom";
+import { render } from "react-dom";
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import NavDrawer from "./components/navDrawer";
 
 const rootElement = document.querySelector("#root");
+
 if (rootElement) {
-  render(<NavDrawer/>, rootElement);
+  render(
+    <Provider store={store}>
+      <NavDrawer/>
+    </Provider>,
+     rootElement);
 }
