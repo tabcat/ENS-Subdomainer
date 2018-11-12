d9c4abe63aa3a1de644a79398c828c39e4549b04
anders <36933094+tabcat@users.noreply.github.com>
2018-11-12 03:11:42 -0600

    EIP 1102 coverage

--- app/store.js
+++ app/store.js
@@ -3,21 +3,23 @@ import { createStore, combineReducers } from 'redux';
 import SubdomainResolver from 'Embark/contracts/SubdomainResolver';
 
 
-let config = {
-  rootDomain: "subdomain",
-  referralAddr: "0x232c1526a71A4fD696Bc4B2B7FAA8698A32eB7Fb",
-  resolverAddr: SubdomainResolver.address
-}
+// let config = {
+//   rootDomain: "subdomain",
+//   referralAddr: "0x232c1526a71A4fD696Bc4B2B7FAA8698A32eB7Fb",
+//   resolverAddr: SubdomainResolver.address
+// }
 
 
 //reducers
 
-function appConfigReducer(state = {}, action) {
+function appConfigReducer(
+  state = {
+    rootDomain: "subdomain",
+    referralAddr: "0x232c1526a71A4fD696Bc4B2B7FAA8698A32eB7Fb",
+    resolverAddr: SubdomainResolver.address
+  },
+  action) {
   switch (action.type) {
-    case 'INIT_SUCCESS':
-    case 'INIT_FAIL':
-      let { rootDomain, referralAddr, resolverAddr } = config;
-      return { ...state, rootDomain, referralAddr, resolverAddr};
     default:
       return state;
   }
@@ -28,100 +30,102 @@ function web3Reducer(state = {}, action) {
   switch (action.type) {
     case 'SET_ADDR':
       return { ...state, currentAcc: action.addr};
-    case 'INIT_SUCCESS':
+    case 'WEB3_SUCCESS':
       return { ...state, web3: true, currentAcc, networkType };
-    case 'INIT_FAIL':
+    case 'WEB3_DISABLED':
+      return { ...state, web3: false, currentAcc: 'web3 not injected', networkType };
+    case 'WEB3_FAIL':
       return { ...state, web3: false};
     default:
       return state;
   }
 }
 
-function navReducer(state = {}, action) {
+function navReducer(state = { selectedDrawer: 0, mobileOpen: false }, action) {
   switch (action.type) {
     case 'SET_DRAWER':
       return { ...state, selectedDrawer: action.selected, mobileOpen: false };
     case 'MOBILE_OPEN':
       return { ...state, mobileOpen: action.mobile };
-    case 'INIT_SUCCESS':
-    case 'INIT_FAIL':
-      return { ...state, selectedDrawer: 0, mobileOpen: false };
     default:
       return state;
   }
 }
 
-function dialoggerReducer(state = { dialogOpen: false }, action) {
+function dialoggerReducer(
+  state = {
+    dialogOpen: false,
+    dialogAction: ''
+  },
+  action) {
   let { dialogOpen, dialogAction } = action;
   switch (action.type) {
     case 'TOGGLE_DIALOG':
       return { ...state, dialogOpen };
     case 'SET_DIALOG':
       return { ...state, dialogOpen, dialogAction };
-    case 'INIT_SUCCESS':
-    case 'INIT_FAIL':
-      return {
-        ...state,
-        dialogOpen: false,
-        dialogAction: ''
-      };
+    case 'WEB3_DISABLED':
+      return { ...state, dialogOpen: true, dialogAction: 'ENABLE_ETH' };
+    case 'WEB3_ENABLED':
+      return { ...state, dialogOpen: false };
     default:
       return state;
   }
 }
 
-function txSnackbarReducer(state = {}, action) {
+function txSnackbarReducer(
+  state = { snackOpen: false, tx: '', error: null },
+  action) {
   let { snackOpen, tx, error } = action;
   switch (action.type) {
     case 'TX_SNACK':
       return { ...state, snackOpen, tx, error };
     case 'CLOSE_SNACK':
       return { ...state, snackOpen: false}
-    case 'INIT_SUCCESS':
-    case 'INIT_FAIL':
-      return { ...state, snackOpen: false, tx: '', error: null };
     default:
       return state;
   }
 }
 
-function subnameReducer(state = {}, action) {
+function subnameReducer(
+  state = {
+    subnameSearch: '',
+    ownable: false,
+    availability: "must be longer than 0 characters :P"
+  },
+  action) {
   let { subnameSearch, ownable, availability } = action;
   switch (action.type) {
     case 'SET_SUBNAME_SEARCH':
       return { ...state, subnameSearch };
     case 'SET_AVAIL':
       return { ...state, ownable, availability };
-    case 'INIT_SUCCESS':
-    case 'INIT_FAIL':
-      return {
-        ...state,
-        subnameSearch: '',
-        ownable: false,
-        availability: "must be longer than 0 characters :P",
-      };
     default:
       return state;
   }
 }
 
-function advancedUtilReducer(state = {}, action) {
+function advancedUtilReducer(state = { currentTab: 0 }, action) {
   let { currentTab } = action;
   switch (action.type) {
     case 'SET_UTIL_TAB':
       return { ...state, currentTab };
-    case 'INIT_SUCCESS':
-    case 'INIT_FAIL':
-      return {
-        ...state,
-        currentTab: 0
-      };
     default:
       return state;
   }
 }
 
-function contractReaderReducer(state = {}, action) {
+function contractReaderReducer(
+  state = {
+    contract: '',
+    method: '',
+    inputs: [],
+    msgValue: '',
+    output: '',
+    outputs: [],
+    pickerState: true
+  },
+  action) {
   let { contract, method, inputs, msgValue, output, outputs, pickerState } = action;
   switch (action.type) {
     case 'SET_INPUTS':
@@ -134,24 +138,19 @@ function contractReaderReducer(state = {}, action) {
       return { ...state, contract, method, inputs, msgValue, pickerState };
     case 'SET_METHOD':
       return { ...state, method, inputs, msgValue, outputs, pickerState };
-    case 'INIT_SUCCESS':
-    case 'INIT_FAIL':
-      return {
-        ...state,
-        contract: '',
-        method: '',
-        inputs: [],
-        msgValue: '',
-        output: '',
-        outputs: [],
-        pickerState: true,
-      };
     default:
       return state;
   }
 }
 
-function ensRegistrarReducer(state = {}, action) {
+function ensRegistrarReducer(
+  state = {
+    nameSearch: '',
+    entry: '',
+    entryState: '',
+    bid: ['','','','']
+  },
+  action) {
   let { nameSearch, entry, entryState, bid, deedOwner } = action;
   switch (action.type) {
     case 'SET_NAME_SEARCH':
@@ -160,34 +159,23 @@ function ensRegistrarReducer(state = {}, action) {
       return { ...state, entry, entryState, deedOwner, bid };
     case 'SET_BID':
       return { ...state, bid }
-    case 'INIT_SUCCESS':
-    case 'INIT_FAIL':
-      return {
-        ...state,
-        nameSearch: '',
-        entry: '',
-        entryState: '',
-        bid: ['','','','']
-      };
     default:
       return state;
   }
 }
 
-function toolboxReducer(state = {}, action) {
+function toolboxReducer(
+  state = {
+    hasherInput: '',
+    expanded: false
+  },
+  action) {
   let { hasherInput, expanded } = action;
   switch (action.type) {
     case 'SET_HASHER_INPUT':
       return { ...state, hasherInput };
     case 'TOGGLE_EXPAND':
       return { ...state, expanded };
-    case 'INIT_SUCCESS':
-    case 'INIT_FAIL':
-      return {
-        ...state,
-        hasherInput: '',
-        expanded: false
-      };
     default:
       return state;
   }
